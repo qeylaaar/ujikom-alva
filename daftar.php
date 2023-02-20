665adf491e92d723f203c218b7ebb0d4aca4c741
qeylaaar <qeylaraiqalva@gmail.com>
2023-02-20 12:39:16 +0700

    step 1 done : userpage

---
+++ daftar.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login | Adukan!</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="assets/index2.html" class="h1">Daftar</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Silahkan Daftar Terlebih dahulu jika tidak memiliki akun</p>
+
+      <form action="assets/index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="NIK">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <form action="assets/index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Nama">
+          <div class="input-group-append">
+          </div>
+        </div>
+      <form action="assets/index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Telepon">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+          <a href="login.php" class="text-center">Sudah Punya Akun</a>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <a href="index.php"><button type="submit" class="btn btn-primary btn-block">Daftar</button></a>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <!-- /.social-auth-links -->
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/dist/js/adminlte.min.js"></script>
+</body>
+</html>

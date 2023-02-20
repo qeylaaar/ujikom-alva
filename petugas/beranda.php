696b16d3a77bb86564e369de0af42c7700a290d8
qeylaaar <qeylaraiqalva@gmail.com>
2023-02-20 15:24:27 +0700

    second step 50%

--- petugas/beranda.php
+++ petugas/beranda.php
@@ -8,14 +8,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Adukan! | Pengaduan Masyarakat</title>
+  <title>Dashboard ( Admin ) | Adukan!</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition layout-top-nav">
 <div class="wrapper">
@@ -24,7 +24,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
       <a href="index.php" class="navbar-brand">
-        <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="../assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Adukan!</span>
       </a>
 
@@ -36,10 +36,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Left navbar links -->
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a href="index.php" class="nav-link">Home</a>
+            <a href="beranda.php" class="nav-link">Home</a>
           </li>
           <li class="nav-item">
-            <a href="pengaduan.php" class="nav-link">Pengaduan</a>
+            <a href="data_pengaduan.php" class="nav-link">Data Pengaduan</a>
           </li>
         </ul>
       </div>
@@ -72,24 +72,41 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- /.content-header -->
 
     <!-- Main content -->
-    <div class="content">
-      <div class="container">
-        <div class="row">
+        <div class="content">
+            <div class="container">
+                <div class="row">
           <!-- /.col-md-6 -->
-          <div class="col-lg-12">
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="card-title m-0">Home</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Welcome to Adukan!</h6>
-
-                <p class="card-text">Di aplikasi ini, kalian bisa menulis aduan kalian, aduan kalian akan menjadi motivasi dan evaluasi bagi kami!, silahkan klik tombol dibawah untuk mulai mengetik aduan kalian</p>
-                <a href="#" class="btn btn-primary">Mulai</a>
-              </div>
+                <div class="col-lg-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h5 class="card-title m-0">Dashboard</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6 col-12">
+                                    <div class="info-box">
+                                        <span class="info-box-icon bg-warning"><i class="fas fa-download"></i></span>
+                                        <div class="info-box-content">
+                                            <span class="info-box-text">Pengaduan Masuk</span>
+                                            <span class="info-box-number">1,410</span>
+                                        </div>
+                                        <!-- /.info-box-content -->
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6 col-12">
+                                    <div class="info-box">
+                                        <span class="info-box-icon bg-success"><i class="fas fa-check"></i></span>
+                                        <div class="info-box-content">
+                                            <span class="info-box-text">Pengaduan Selesai</span>
+                                            <span class="info-box-number">1,410</span>
+                                        </div>
+                                        <!-- /.info-box-content -->
+                                </div>
+                            </div>
+                    </div>
+                </div>
             </div>
-          </div>
+        </div>
           <!-- /.col-md-6 -->
         </div>
         <!-- /.row -->
@@ -120,12 +137,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="assets/dist/js/adminlte.min.js"></script>
+<script src="../assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="assets/dist/js/demo.js"></script>
+<script src="../assets/dist/js/demo.js"></script>
 </body>
 </html>
